f3575ea6bb8f16646a5b90d30884447ccd840dac
Anwesh43 <anweshthecool0@gmail.com>
2020-09-23 00:44:04 +0530

    adding constants and libraries

--- openboxmoverview/src/main/java/com/anwesh/uiprojects/openboxmoverview/OpenBoxMoverView.kt
+++ openboxmoverview/src/main/java/com/anwesh/uiprojects/openboxmoverview/OpenBoxMoverView.kt
@@ -3,3 +3,25 @@ package com.anwesh.uiprojects.openboxmoverview
 /**
  * Created by anweshmishra on 23/09/20.
  */
+
+import android.content.Context
+import android.app.Activity
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#009688",
+        "#2196F3",
+        "#4CAF50",
+        "#FFC107"
+).map({Color.parseColor(it)}).toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 8.9f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
