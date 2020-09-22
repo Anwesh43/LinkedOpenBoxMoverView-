5bd4896c5d448149fc0ffd60bc442f85f2bf7a0c
Anwesh43 <anweshthecool0@gmail.com>
2020-09-23 00:54:03 +0530

    adding Canvas extension methods

--- openboxmoverview/src/main/java/com/anwesh/uiprojects/openboxmoverview/OpenBoxMoverView.kt
+++ openboxmoverview/src/main/java/com/anwesh/uiprojects/openboxmoverview/OpenBoxMoverView.kt
@@ -30,3 +30,39 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawOpenBoxMover(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val size : Float = w / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(- w / 2, -size / 2)
+        for (i in 0..1) {
+            save()
+            translate(0f, size * i)
+            drawLine(w / 2, 0f, w / 2 - (w / 2) * sf1 + (w / 2 - size) * sf3, 0f, paint)
+            restore()
+        }
+        save()
+        translate(-w / 2 + size + (w / 2 - size) * sf3, -size / 2)
+        drawLine(0f, 0f, 0f, size * sf2, paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawOBMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawOpenBoxMover(scale, w, h, paint)
+}
